a3e542ab1719e897d9a1c3966ed709b4b35795ef
Steve Riesenberg <sriesenberg@vmware.com>
2023-08-16 13:47:14 -0500

    Multi-module project

---
+++ auth-server/src/test/java/academy/spring/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,14 @@
+package academy.spring.authserver;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
