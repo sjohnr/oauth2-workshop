c75361c99542e39d7022de9565ac3deeb4fa3a61
Josh Cummings <josh.cummings@gmail.com>
2023-08-22 16:56:30 -0600

    Step 5: Filter By Owner
    Spring Security can integrate with Spring Data queries as
    well to ensure that the current Authentication is considered where
    appropriate.

--- api/src/main/java/academy/spring/cashcard/CashCardRepository.java
+++ api/src/main/java/academy/spring/cashcard/CashCardRepository.java
@@ -1,6 +1,11 @@
 package academy.spring.cashcard;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
 
 public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+	@Query("SELECT * FROM cash_card cc WHERE cc.owner = :#{authentication.name}")
+	@NonNull
+	Iterable<CashCard> findAll();
 }
