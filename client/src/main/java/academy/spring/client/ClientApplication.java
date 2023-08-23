ef24ef41213a553d6b7745e41f6b30accbde61bd
Steve Riesenberg <sriesenberg@vmware.com>
2023-08-23 13:19:11 -0700

    Step 18a (optional): Persist authorizations in the database

--- client/src/main/java/academy/spring/client/ClientApplication.java
+++ client/src/main/java/academy/spring/client/ClientApplication.java
@@ -3,6 +3,9 @@ package academy.spring.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.r2dbc.core.DatabaseClient;
+import org.springframework.security.oauth2.client.R2dbcReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
@@ -21,6 +24,14 @@ public class ClientApplication {
 
 		return new ServerOAuth2AuthorizedClientExchangeFilterFunction(
 				clientRegistrationRepository, authorizedClientRepository);
+	}
+
+	@Bean
+	public ReactiveOAuth2AuthorizedClientService authorizedClientService(
+			DatabaseClient db,
+			ReactiveClientRegistrationRepository clientRegistrationRepository) {
+
+		return new R2dbcReactiveOAuth2AuthorizedClientService(db, clientRegistrationRepository);
 	}
 
 }
