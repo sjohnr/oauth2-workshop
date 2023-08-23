3480a857e955eeedea38e34de7484195a075c52b
Steve Riesenberg <sriesenberg@vmware.com>
2023-08-23 11:51:33 -0700

    Step 15: Use ExchangeFilterFunction

--- client/src/main/java/academy/spring/client/CashCardController.java
+++ client/src/main/java/academy/spring/client/CashCardController.java
@@ -2,32 +2,31 @@ package academy.spring.client;
 
 import reactor.core.publisher.Mono;
 
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
+
 @RestController
-class CashCardController {
+public class CashCardController {
 
 	private final WebClient webClient;
 
-	CashCardController(WebClient.Builder webClientBuilder) {
+	public CashCardController(WebClient.Builder webClientBuilder,
+		ServerOAuth2AuthorizedClientExchangeFilterFunction oauth2) {
 		this.webClient = webClientBuilder
 			.baseUrl("http://localhost:8090")
+			.filter(oauth2)
 			.build();
 	}
 
 	@GetMapping("/cashcards")
-	Mono<CashCard[]> getCashCards(
-		@RegisteredOAuth2AuthorizedClient("oidc-client")
-		OAuth2AuthorizedClient authorizedClient) {
-
-		String accessToken = authorizedClient.getAccessToken().getTokenValue();
+	public Mono<CashCard[]> getCashCards() {
 		return this.webClient.get()
 			.uri("/cashcards")
-			.headers(headers -> headers.setBearerAuth(accessToken))
+			.attributes(clientRegistrationId("oidc-client"))
 			.retrieve()
 			.bodyToMono(CashCard[].class);
 	}
--- client/src/main/java/academy/spring/client/ClientApplication.java
+++ client/src/main/java/academy/spring/client/ClientApplication.java
@@ -2,12 +2,25 @@ package academy.spring.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 
 @SpringBootApplication
 public class ClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
+	}
+
+	@Bean
+	public ServerOAuth2AuthorizedClientExchangeFilterFunction oauth2(
+			ReactiveClientRegistrationRepository clientRegistrationRepository,
+			ServerOAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+		return new ServerOAuth2AuthorizedClientExchangeFilterFunction(
+				clientRegistrationRepository, authorizedClientRepository);
 	}
 
 }
