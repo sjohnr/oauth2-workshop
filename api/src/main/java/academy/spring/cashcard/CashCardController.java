4a420912829fe9f03a960d977c3140ae0030f3a6
Josh Cummings <josh.cummings@gmail.com>
2023-08-24 09:38:51 -0600

    Remove Stray Package Import

--- api/src/main/java/academy/spring/cashcard/CashCardController.java
+++ api/src/main/java/academy/spring/cashcard/CashCardController.java
@@ -3,7 +3,6 @@ package academy.spring.cashcard;
 import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
