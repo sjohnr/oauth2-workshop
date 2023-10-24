24fbbffc6a45d643e684aa9fff6d6dea62f7b0a6
Steve Riesenberg <sriesenberg@vmware.com>
2023-10-23 22:16:52 -0500

    Add mobile-client

---
+++ mobile-client/src/main/java/academy/spring/mobileclient/MobileClientApplication.java
@@ -0,0 +1,13 @@
+package academy.spring.mobileclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileClientApplication.class, args);
+	}
+
+}
---
+++ mobile-client/src/test/java/academy/spring/mobileclient/MobileClientApplicationTests.java
@@ -0,0 +1,14 @@
+package academy.spring.mobileclient;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
