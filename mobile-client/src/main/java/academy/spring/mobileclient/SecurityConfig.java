4f852d2ab71e225ecb37cca3d255bbc11ceeeda2
Steve Riesenberg <sriesenberg@vmware.com>
2023-10-23 22:25:50 -0500

    Add OIDC client-initiated logout

--- mobile-client/src/main/java/academy/spring/mobileclient/SecurityConfig.java
+++ mobile-client/src/main/java/academy/spring/mobileclient/SecurityConfig.java
@@ -5,16 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
-import org.springframework.security.web.server.authentication.logout.HttpStatusReturningServerLogoutSuccessHandler;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
 	@Bean
-	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http,
+			ReactiveClientRegistrationRepository clientRegistrationRepository) {
 
 		http
 			.authorizeExchange((authorize) -> authorize
@@ -26,9 +28,13 @@ public class SecurityConfig {
 				.authenticationSuccessHandler(
 					new SecurityContextIdRedirectServerAuthenticationSuccessHandler("/callback"))
 			)
-			.logout((logout) -> logout
-				.logoutSuccessHandler(new HttpStatusReturningServerLogoutSuccessHandler())
-			)
+			.logout((logout) -> {
+				OidcClientInitiatedServerLogoutSuccessHandler logoutSuccessHandler =
+					new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository);
+				logoutSuccessHandler.setPostLogoutRedirectUri("http://127.0.0.1:8080/");
+
+				logout.logoutSuccessHandler(logoutSuccessHandler);
+			})
 			.securityContextRepository(new InMemoryServerSecurityContextRepository())
 			.exceptionHandling((exceptions) -> exceptions
 				.authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))
