a013c12800af1b0007190d1c5325cf1802098e57
Steve Riesenberg <5248162+sjohnr@users.noreply.github.com>
2024-07-03 15:15:08 -0500

    Add token-exchange to gateway
    This commit demonstrates bypassing the "monolith" by directly using
    token-exchange from the gateway to obtain an access token with the
    subject of the current user. The idea here is to incrementally improve
    the architecture by:
    
    1. Creating a new microservice called by the monolith but leaving the
       gateway unchanged.
    2. Eventually changing the gateway to bypass the monolith and call the
       microservice directly.
    
    Note that the `aud` claim of the token obtained by the gateway is still
    correct for the new microservice.
    
    Note also that with this change, the gateway no longer requests scopes
    other than during login. Use of authorization grants become an internal
    implementation detail hidden from the user.

--- client/src/main/java/academy/spring/client/SecurityConfig.java
+++ client/src/main/java/academy/spring/client/SecurityConfig.java
@@ -1,14 +1,28 @@
 package academy.spring.client;
 
+import java.util.function.Function;
+
+import reactor.core.publisher.Mono;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizationContext;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.R2dbcReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.TokenExchangeReactiveOAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.OAuth2Token;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationEntryPoint;
 
@@ -37,6 +51,57 @@ public class SecurityConfig {
 			DatabaseClient db, ReactiveClientRegistrationRepository clientRegistrationRepository) {
 
 		return new R2dbcReactiveOAuth2AuthorizedClientService(db, clientRegistrationRepository);
+	}
+
+	@Bean
+	public ReactiveOAuth2AuthorizedClientManager authorizedClientManager(
+			ReactiveClientRegistrationRepository clientRegistrationRepository,
+			ServerOAuth2AuthorizedClientRepository authorizedClientRepository,
+			ReactiveOAuth2AuthorizedClientProvider tokenExchange) {
+
+		ReactiveOAuth2AuthorizedClientProvider authorizedClientProvider =
+				ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
+						.authorizationCode()
+						.refreshToken()
+						.provider(tokenExchange)
+						.build();
+
+		DefaultReactiveOAuth2AuthorizedClientManager authorizedClientManager =
+				new DefaultReactiveOAuth2AuthorizedClientManager(
+						clientRegistrationRepository, authorizedClientRepository);
+		authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+		return authorizedClientManager;
+	}
+
+	@Bean
+	public TokenExchangeReactiveOAuth2AuthorizedClientProvider tokenExchange(
+			ReactiveClientRegistrationRepository clientRegistrationRepository,
+			ServerOAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+		DefaultReactiveOAuth2AuthorizedClientManager authorizedClientManager =
+				new DefaultReactiveOAuth2AuthorizedClientManager(
+						clientRegistrationRepository, authorizedClientRepository);
+
+		TokenExchangeReactiveOAuth2AuthorizedClientProvider authorizedClientProvider =
+				new TokenExchangeReactiveOAuth2AuthorizedClientProvider();
+		authorizedClientProvider.setSubjectTokenResolver(createTokenResolver(authorizedClientManager, "oidc-client"));
+
+		return authorizedClientProvider;
+	}
+
+	private static Function<OAuth2AuthorizationContext, Mono<OAuth2Token>> createTokenResolver(
+			ReactiveOAuth2AuthorizedClientManager authorizedClientManager, String clientRegistrationId) {
+
+		return (context) -> {
+			OAuth2AuthorizeRequest authorizeRequest =
+					OAuth2AuthorizeRequest.withClientRegistrationId(clientRegistrationId)
+							.principal(context.getPrincipal())
+							.build();
+
+			return authorizedClientManager.authorize(authorizeRequest)
+					.map(OAuth2AuthorizedClient::getAccessToken);
+		};
 	}
 
 }
